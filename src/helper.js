e5bbceea93358f738ce9257f71cf1c756283ea26
Ben Porter <bbp5280@gmail.com>
2017-10-03 12:11:16 -0600

    adjusted the reduce so the data is a bit more clean

--- src/helper.js
+++ src/helper.js
@@ -6,9 +6,15 @@ export default class DistrictRepository {
       if (!acc[obj.Location]){
         acc[obj.Location]= [];
       }
-        acc[obj.Location].push(obj);
+        acc[obj.Location].push({ year: obj.TimeFrame,
+          percent: obj.Data} );
         return acc;
     }, {});
+    console.log(this.data);
   }
-  
+
+  findByName(){
+    Object.keys(this.data)
+  }
+
 }
