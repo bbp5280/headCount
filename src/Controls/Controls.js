e2fedc45261e7600fec6c676a00af068a42de254
Ben Porter <bbp5280@gmail.com>
2017-10-04 18:12:17 -0600

    render search and almost works

---
+++ src/Controls/Controls.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Controls = ({handleSearch}) =>{
+return(
+<div>
+  <input type='text' placeholder='Search Districts' onChange={handleSearch}/>
+</div>
+)
+}
+
+
+export default Controls;
