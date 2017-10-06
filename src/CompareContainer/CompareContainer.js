793847e68b56d0edf102e1dfccfe95465f2892ba
Ben Porter <bbp5280@gmail.com>
2017-10-06 09:49:59 -0600

    fixed merged

--- src/CompareContainer/CompareContainer.js
+++ src/CompareContainer/CompareContainer.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import Card from '../Card/Card';
+import DistrictRepository from '../helper'
+import kinderData from './../../data/kindergartners_in_full_day_program.js';
+
 
 class CompareContainer extends Component {
-  // constructor() {
-  //   super();
-  //
-  // }
+  constructor() {
+    super();
+    this.state = {
+      compareData: {}
+    }
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.compareData(nextProps)
+  }
+
+  compareData(props){
+    const district1 = props.compareArray[0]
+    const district2 = props.compareArray.splice(0,1)
+    const obj = Object.assign({}, district1)
+    console.log(obj.location)
+
+    const district = new DistrictRepository(kinderData)
+    // const averages = district.compareDistrictAverages(district1, district2 )
+      // console.log(averages);
+  }
+
+//move or call this in props change life cycle
+
+// will render or mount thing when all parts are there only
+
+//possibly use prep dom for render to get things in right order
 
   render() {
+
     const comparedDistricts = this.props.compareArray.map ( district => {
       return <Card location={district.location.location}
                    data={district.data.data}
