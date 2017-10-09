d1ba44be19162b4416a97ab9492a2537450cc40f
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-10-08 22:10:26 -0600

    set up scroll and control tests

--- src/ScrollBtn/ScrollBtn.test.js
+++ src/ScrollBtn/ScrollBtn.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Controls from './Controls';
+import ScrollBtn from './ScrollBtn';
 import { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 import config from '../test/setup.js';
 
-describe('CONTROLS', () => {
+describe('SCROLL BUTTON', () => {
 
   it('', () => {
 
