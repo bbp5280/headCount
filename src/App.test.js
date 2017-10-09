11e9bc952460f2549d7ce2915b6a53fbc1e96d38
Ben Porter <bbp5280@gmail.com>
2017-10-09 08:44:58 -0600

    add testing and pass all test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,8 @@ import App from './App';
 import { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 import config from '../test/setup.js';
+import DistrictRepository from './helper';
+
 
 describe('APP', () => {
 
@@ -13,7 +15,7 @@ describe('APP', () => {
     expect(newApp.hasClass('parent')).toEqual(true);
   });
 
-  it('is stateful', () => {
+  it('has a state of compareArray', () => {
     const newApp = mount(<App />);
 
     expect(newApp.state('compareArray')).toEqual([]);
@@ -22,6 +24,31 @@ describe('APP', () => {
     expect(newApp.state('compareArray')).toEqual(['COLORADO']);
   });
 
+  it('has a state of allData of 181 in length', () => {
+    const newApp = mount(<App />);
+
+    expect(newApp.state('allData').length).toEqual(181);
+  });
+
+  it('has a state of compareArray', () => {
+    const newApp = mount(<App />);
+
+    expect(newApp.state('comparisonData')).toEqual({});
+    const compareBtn = newApp.find('.compare-btn').first();
+    compareBtn.simulate('click');
+    const compareBtn2 = newApp.find('.compare-btn').last();
+    compareBtn2.simulate('click');
+
+    expect(newApp.state('comparisonData')).toEqual({COLORADO: 0.53,
+                                                    'YUMA SCHOOL DISTRICT 1': 0.909,
+                                                    compared:0.583});
+  });
+
+  it('has a state of compareArray', () => {
+    const newApp = shallow(<App />);
+
+    expect(newApp).toMatchSnapshot();
+  });
 
 
 });
